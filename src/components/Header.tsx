5e51cfe0f84b383103b143e4c07abc96c21c7cb6
guibbsss <39602800+guibbsss@users.noreply.github.com>
2025-04-07 11:45:40 -0300

    Header
    adicionando o header

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo"></div>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">Rotas</a>
+        <a href="#">Comprar</a>
+        <a href="#">Contato</a>
+        <button>Entrar</button>
+      </nav>
+    </header>
+
+  );
+};
+
+export default Header;
